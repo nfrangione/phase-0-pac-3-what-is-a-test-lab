31550473de8f403b3e2e2ee79001fe4af7745db1
Nicholas V Frangione <nfrangione8@gmail.com>
2021-02-18 12:45:25 -0800

    Completed Assignment

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const name = "Joe";
-const height = 74;
-const message = `${name} is ${height} inches tall`;
+var name = "Susan"
+var height = 39
+var message = `${name} is ${height} inches tall`
 
-module.exports = {name, height, message};
+module.exports = {name, height, message}
